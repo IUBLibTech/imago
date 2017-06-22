f1aa36de431b542fc40ba90c0ae130b9786264e8
James Halliday <jhallida@indiana.edu>
2017-06-22 14:14:44 -0400

    remove ETR lists

--- lib/tasks/herbarium_system_check.rb
+++ lib/tasks/herbarium_system_check.rb
@@ -12,7 +12,7 @@ module Cbrc
         #initialize arrays
         idsAll = Array[]
         sdaAll = Array[]
-        etrAll = Array[]
+#        etrAll = Array[]
         symbiotaAll = Array[]
 
         #read in SDA file
@@ -22,10 +22,10 @@ module Cbrc
         end
 
         #read in ETR file
-        puts "READING IN ETR FILE"
-        File.readlines(Rails.root.join('public', 'etr_all.txt')).each do |line|
-          etrAll.push(line.strip)
-        end
+#        puts "READING IN ETR FILE"
+#        File.readlines(Rails.root.join('public', 'etr_all.txt')).each do |line|
+#          etrAll.push(line.strip)
+#        end
 
         #read in Symbiota file
         puts "READING IN SYMBIOTA FILE"
@@ -52,7 +52,7 @@ module Cbrc
         imago_not_sda = idsAll - sdaAll
         sda_not_imago = (sdaAll - idsAll) - etrAll
         symbiota_not_sda = symbiotaAll - sdaAll
-        etr_and_imago = etrAll & idsAll
+ #       etr_and_imago = etrAll & idsAll
 
         puts ("GENERATING HTML AND TEXT FILES");
         #delete any existing system check files
@@ -65,16 +65,16 @@ module Cbrc
         if Rails.root.join('public', 'symbiota_not_sda.txt').exist?
           File.delete(Rails.root.join('public', 'symbiota_not_sda.txt'))
         end
-        if Rails.root.join('public', 'etr_and_imago.txt').exist?
-          File.delete(Rails.root.join('public', 'etr_and_imago.txt'))
-        end
+  #      if Rails.root.join('public', 'etr_and_imago.txt').exist?
+  #        File.delete(Rails.root.join('public', 'etr_and_imago.txt'))
+  #      end
         #create new system check files and open them for writing
         check_html = File.new(Rails.root.join('public', 'herbariumcheck.html'), 'w')
         imago_all = File.new(Rails.root.join('public', 'imago_all.txt'), 'w')
         file_imago_not_sda = File.new(Rails.root.join('public', 'imago_not_sda.txt'), 'w')
         file_sda_not_imago = File.new(Rails.root.join('public', 'sda_not_imago.txt'), 'w')
         file_symbiota_not_sda = File.new(Rails.root.join('public', 'symbiota_not_sda.txt'), 'w')
-        file_etr_and_imago = File.new(Rails.root.join('public', 'etr_and_imago.txt'), 'w')
+#        file_etr_and_imago = File.new(Rails.root.join('public', 'etr_and_imago.txt'), 'w')
 
         #writing html file
         check_html.puts("<html><head><title>HERBARIUM SYSTEM CHECK</title></head><body>")
@@ -82,15 +82,15 @@ module Cbrc
         check_html.puts("<p>Date of System Check: #{Time.now}</p>")
         check_html.puts("<p>SDA list was generated on: #{File.mtime(Rails.root.join('public', 'sda_all.txt'))}</p>")
         check_html.puts("<p>Symbiota list was generated on: #{File.mtime(Rails.root.join('public', 'symbiota_all.txt'))}</p>")
-        check_html.puts("<p>ETR list was generated on: #{File.mtime(Rails.root.join('public', 'etr_all.txt'))}</p>")
+ #       check_html.puts("<p>ETR list was generated on: #{File.mtime(Rails.root.join('public', 'etr_all.txt'))}</p>")
         check_html.puts("<p><a href='imago_all.txt'>List all items in Imago</a> Total: #{idsAll.size}</p>")
         check_html.puts("<p><a href='sda_all.txt'>List all items in SDA</a> Total: #{sdaAll.size}</p>")
         check_html.puts("<p><a href='sda_not_imago.txt'>List all items in SDA not in Imago (excluding ETR)</a> Total: #{sda_not_imago.size}</p>")
         check_html.puts("<p><a href='imago_not_sda.txt'>List all items in Imago not SDA</a> Total: #{imago_not_sda.size}</p>")
         check_html.puts("<p><a href='symbiota_all.txt'>List all items in Symbiota</a> Total: #{symbiotaAll.size}</p>")
         check_html.puts("<p><a href='symbiota_not_sda.txt'>List all items in Symbiota NOT in SDA</a> Total: #{symbiota_not_sda.size}</p>")
-        check_html.puts("<p><a href='etr_all.txt'>List all items on ETR list</a> Total: #{etrAll.size}</p>")
-        check_html.puts("<p><a href='etr_and_imago.txt'>List all items on ETR list that are in Imago</a> Total: #{etr_and_imago.size}</p>")
+  #      check_html.puts("<p><a href='etr_all.txt'>List all items on ETR list</a> Total: #{etrAll.size}</p>")
+  #      check_html.puts("<p><a href='etr_and_imago.txt'>List all items on ETR list that are in Imago</a> Total: #{etr_and_imago.size}</p>")
         check_html.close
 
         #write list of all files
@@ -118,10 +118,10 @@ module Cbrc
         file_symbiota_not_sda.close
 
         #file in ETR and also Imago
-        etr_and_imago.each do |id|
-          file_etr_and_imago.puts(id)
-        end
-        file_etr_and_imago.close
+   #     etr_and_imago.each do |id|
+   #       file_etr_and_imago.puts(id)
+   #     end
+   #     file_etr_and_imago.close
 
       end
     end
