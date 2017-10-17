448b3f738b583ab5e19b02de783eef4a7222749f
James Halliday <jhallida@indiana.edu>
2017-10-17 14:21:49 -0400

    fix password reset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,11 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # Code is not reloaded between requests.
+ # Code is not reloaded between requests.
   config.cache_classes = true
+
+  # set up mailer for sending password reset emails
+  config.action_mailer.default_url_options = { :host => 'imago.indiana.edu' }
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
@@ -29,7 +32,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
